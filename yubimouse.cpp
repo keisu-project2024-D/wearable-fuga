b7cd6681a1e75d65e4ce6cde74d5a0e95168b124
ciesle <ciesle.dev@gmail.com>
2024-07-08 16:43:02 +0900

    Leonardo

--- yubimouse.cpp
+++ yubimouse.cpp
@@ -3,10 +3,11 @@ ADNS2083 Optical1 = ADNS2083(SCLK, SDIO);
 //BleMouse Mouse;
 
 
-void mouseSetup() { 
+void mouseSetup() {
   Optical1.begin();
   Optical1.config(0x00);
   Mouse.begin();
+  Serial.begin(9600);
 
   pinMode(LEFT, INPUT_PULLUP);
   pinMode(RIGHT, INPUT_PULLUP);
@@ -16,108 +17,107 @@ void mouseSetup() {
 
 
 
-void mouseScroll(double now){
-  static double sum=0;
-  sum+=now;
-  if(abs(sum)>=1) {
-    Mouse.move(0,0,floor(sum));
-    sum=sum-floor(sum);
+void mouseScroll(double now) {
+  static double sum = 0;
+  sum += now;
+  if (abs(sum) >= 1) {
+    Mouse.move(0, 0, floor(sum));
+    sum = sum - floor(sum);
   }
 }
 
-void mouseControl(){
+void mouseControl() {
   char x = 0, y = 0, r = 0;
-  bool exist=(Optical1.mo()&(1<<7))>0;
-  if(exist){
+  bool exist = (Optical1.mo() & (1 << 7)) > 0;
+  if (exist) {
     x = Optical1.dx();
     y = -Optical1.dy();
   }
-  
-  bool left=digitalRead(LEFT)==LOW, right=digitalRead(RIGHT)==LOW;
+  //Serial.println((int)(x));
+  //return;
+  bool left = digitalRead(LEFT) == LOW, right = digitalRead(RIGHT) == LOW;
   sendInformation(x, y, left, right);
 }
-void sendInformation(char x, char y, bool left, bool right){
+void sendInformation(char x, char y, bool left, bool right) {
   //クリックの安定性向上
-  static long leftBegin=-1, rightBegin=-1;
-  static bool leftPressing=false, rightPressing=false;
-  const static char history_size=7;
-  static bool history[2][7]={{false,false,false,false,false,false,false},{false,false,false,false,false,false,false}};
-  static int count[2]={0,0},cursor=0;
-  
-  static long last_click[2]={-1,-1};
-  const long last_click_thres=300;
-  const int loop_time_estimate=6;
-  const long click_thres=100;
+  static long leftBegin = -1, rightBegin = -1;
+  static bool leftPressing = false, rightPressing = false;
+  const static char history_size = 7;
+  static bool history[2][7] = { { false, false, false, false, false, false, false }, { false, false, false, false, false, false, false } };
+  static int count[2] = { 0, 0 }, cursor = 0;
 
-  static int roll_val=0;
-  long decrease_count=0;
+  static long last_click[2] = { -1, -1 };
+  const long last_click_thres = 300;
+  const int loop_time_estimate = 6;
+  const long click_thres = 200;
 
-  long now=millis();
+  static int roll_val = 0;
+  long decrease_count = 0;
 
-  for(int i=0;i<2;i++){
-    bool tmp=left;
-    if(i==1) tmp=right;
-    if(tmp&&!history[i][cursor]) count[i]++;
-    if(!tmp&&history[i][cursor]) count[i]--;
-    history[i][cursor]=tmp;
+  long now = millis();
+
+  for (int i = 0; i < 2; i++) {
+    bool tmp = left;
+    if (i == 1) tmp = right;
+    if (tmp && !history[i][cursor]) count[i]++;
+    if (!tmp && history[i][cursor]) count[i]--;
+    history[i][cursor] = tmp;
   }
-  (cursor+=1)%=history_size;
+  (cursor += 1) %= history_size;
 
-  if(left) {
-    if(leftBegin<0) leftBegin=now;
+  if (left) {
+    if (leftBegin < 0) leftBegin = now;
   }
-  if(~leftBegin && now-leftBegin>loop_time_estimate*history_size){
-    if(count[0]<=2){
-      if(!leftPressing){
-        if(now-last_click[0]>last_click_thres){
-          Serial.println("click");
+  if (~leftBegin && now - leftBegin > loop_time_estimate * history_size) {
+    if (count[0] <= 2) {
+      if (!leftPressing) {
+        if (now - last_click[0] > last_click_thres) {
+          //Serial.println("click");
           Mouse.click(MOUSE_LEFT);
-          last_click[0]=now;
-          leftBegin=-1;
+          last_click[0] = now;
+          leftBegin = -1;
         }
-      }
-      else{
-        Serial.println("release");
+      } else {
+        //Serial.println("release");
         Mouse.release(MOUSE_LEFT);
-        leftPressing=false;
-        leftBegin=-1;
+        leftPressing = false;
+        leftBegin = -1;
       }
     }
-    if(count[0]>=history_size-2&&!leftPressing&&now-leftBegin>=click_thres){
+    if (count[0] >= history_size - 2 && !leftPressing && now - leftBegin >= click_thres) {
       Mouse.press(MOUSE_LEFT);
-      leftPressing=true;
-      Serial.println("press");
+      leftPressing = true;
+      //Serial.println("press");
     }
-  }
-  
-
-  if(right) {
-    if(rightBegin<0) rightBegin=now;
-  }
-  if(count[1]>=history_size-2){
-    rightPressing=true;
-    rightBegin=-1;
-  }
-  else {
-    if(rightPressing){
-      rightPressing=false;
-    }
-  }
-  if(~rightBegin&&now-rightBegin>=click_thres){
-    if(now-last_click[1]>last_click_thres){
-      Mouse.click(MOUSE_RIGHT);
-      last_click[1]=now;
-    }
-    rightBegin=-1;
   }
 
-  if(abs(x)>0||abs(y)>0){
-    if(rightPressing){
-      mouseScroll(-y/20.);
+  if (right) {
+    if (rightBegin < 0) rightBegin = now;
+  }
+  if (~rightBegin && now - rightBegin > loop_time_estimate * history_size) {
+    if (count[1] <= 3) {
+      if (!rightPressing) {
+        if (now - last_click[1] > last_click_thres) {
+          Mouse.click(MOUSE_RIGHT);
+          last_click[1] = now;
+          rightBegin = -1;
+        }
+      } else {
+        rightPressing = false;
+        rightBegin = -1;
+      }
+    }
+    if (count[1] >= history_size - 3 && !rightPressing && now - rightBegin >= click_thres) {
+      rightPressing = true;
+    }
+  }
+
+  if (abs(x) > 0 || abs(y) > 0) {
+    if (rightPressing) {
+      mouseScroll(-y / 30.);
       //Serial.println((int)now);
-    }
-    else{
-      Mouse.move(x,y);
+    } else {
+      Mouse.move(x, y);
     }
   }
 }
--- yubimouse.h
+++ yubimouse.h
@@ -3,9 +3,9 @@
 #include "Mouse.h"
 #define ENCODER_DO_NOT_USE_INTERRUPTS
 #define SCLK 2                            // Serial clock pin on the Arduino
-#define SDIO 3                            // Serial data (I/O) pin on the Arduino
-#define LEFT 4
-#define RIGHT 5
+#define SDIO 4                            // Serial data (I/O) pin on the Arduino
+#define LEFT 16
+#define RIGHT 17
 
 void mouseSetup();
 void mouseScroll(double now);
